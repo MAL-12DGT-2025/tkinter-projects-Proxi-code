a44e94226b537ec4df77a054e87335dabf542a35
Proxi-code <juvel.jaison@marcellin.school.nz>
2025-03-04 15:14:46 +1300

    Added more code

--- Temperature.py
+++ Temperature.py
@@ -1,6 +1,11 @@
 import tkinter as tk 
 import tkinter as Tk
 
-root = Tk,tk()
+root = tk,Tk()
+
+title = tk.Label(root, text ="Temperture\nConverter")
+title.grid(row=0, column=0)
+number = Tk.Entry(root)
+number.grid(row = 1, column=0)
 
 root.mainloop()
